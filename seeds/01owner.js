64ad8ffe98938329a85e93788d019906e6b6cbd0
“Andrew <abiv23@gmail.com>
2017-01-26 11:05:21 -0700

    prep for delpoyment

--- seeds/01owner.js
+++ seeds/01owner.js
@@ -1,15 +1,18 @@
+var bcrypt = require('bcrypt');
+
 exports.seed = function(knex, Promise) {
     // Deletes ALL existing entries
     return knex.raw('DELETE FROM owner; ALTER SEQUENCE owner_id_seq RESTART WITH 3')
         .then(() => {
+            const salt = bcrypt.genSaltSync(10);
             const owners = [{
                 id: 1,
                 email: 'abiv@gmail.com',
-                password: 'salthash'
+                password: bcrypt.hashSync('salthash', salt)
             }, {
                 id: 2,
                 email: 'baiv@gmail.colName',
-                password: 'hashsalt'
+                password: bcrypt.hashSync('hashsalt', salt)
             }]
             return knex('owner').insert(owners);
         });
